0479be34f4552a4e16aabb76d5901e2eabb0ed04
Katuishi <diogenes_mj@hotmail.com>
2020-04-16 21:44:26 -0400

    fea: sign up

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import {  actionType} from "../Redux/typeAction";
+
 
 export const Signup = (_) => {
+
+  const dispatch = useDispatch()
+  const history = useHistory()
+
+  const signUpUser = (username) => dispatch(()=>{
+    dispatch({
+      type: actionType.SIGN_UP_USER,
+      user:username
+    })
+  })
+
+  const get_username = (e) =>{
+    e.preventDefault()
+    signUpUser(e.target.username.value)
+    history.push('/chat')
+  }
   return (
-    <form className ="form-sign-up" onSubmit={"return false"}>
+    <form  onSubmit={(e)=> get_username(e)} className="form-sign-up" >
       <p>Sign Up</p>
-      <input minLength={"4"} maxLength = {"18"} placeholder="Type your nick" required/>
+      <input name="username" minLength={"4"} maxLength = {"18"} placeholder="Type your nick" required/>
       <button>Sign Up</button>
     </form>  
   );
