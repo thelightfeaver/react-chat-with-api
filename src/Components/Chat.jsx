a4ab5802339bfeb3a2cd8151e8f0b6e2d872cbdd
Katuishi <diogenes_mj@hotmail.com>
2020-04-16 21:41:45 -0400

    fea: send and get message

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,34 +1,67 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Message } from "./Message";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import  * as icon   from "@fortawesome/free-solid-svg-icons";
-
+import { useDispatch,useSelector } from "react-redux";
+import { fetchSendMessage, fetchReceiveMessage } from "../Redux/MessageAction";
 export const Chat = _ =>{
+
+
+    const text = useRef('')
+    const dispatch = useDispatch()
+    const userState = useSelector(state => state.user)
+    const stateMessages = useSelector(state => state.message)
+    
+    const sendMessage = msg => dispatch(fetchSendMessage(msg))
+    const getMessage = _ => dispatch(fetchReceiveMessage())
+    
+    const handleClick = msg =>{
+
+        const new_msg = {
+            'user':userState,
+            'text':msg.value
+        }
+        sendMessage(new_msg)
+        msg.value = ""
+        getMessage()
+    }
+    
+    
+     
+    
+
     return(
         <div className="container-chat">
+
             <div className="box-title">
                 <p>Global Chat</p>
-            </div>
-            <div className="box-chat">
-                <Message data = { { me : false,text : "algo buena ba pasar en tu vida",user:"katuishi" }}/>
-                <Message data = { { me : true,text : "algo buena ba pasar en tu vida",user:"katuishi" }}/>
-                <Message data = { { me : true,text : "algo buena ba pasar en tu vida",user:"katuishi" }}/>
-                <Message data = { { me : false,text : "algo buena ba pasar en tu vida",user:"katuishi" }}/>
-                <Message data = { { me : true,text : "algo ",user:"katuishi" }}/>
-                <Message data = { { me : true,text : "algo buena ba pasar en tu vida",user:"katuishi" }}/>
-                <Message data = { { me : false,text : "algo buena ba pasar en tu vida",user:"katuishi" }}/>
-                <Message data = { { me : true,text : "algo buena ba pasar en tu vida",user:"katuishi" }}/>
-                <Message data = { { me : true,text : ", algo buena ba pasar en tu vida algo buena ba pasar en tu vida algo buena ba pasar en tu vida algo buena ba pasar en tu vida algo buena ba pasar en tu vida",user:"katuishi" }}/>
                 
+            </div>
 
+            <div className="box-chat">
+               
+            
+
+                {
+                    stateMessages.data.lenght === 0 ? 
+                    (false)
+                    :
+                    (
+                        stateMessages.data.map((e,i)=>{
+                            return(<Message key={i} data={e}/>)
+                        })
+                    )
+                }
+                
             </div>
             <div className="container-control">
-                <textarea className="inp-msg" cols="5" rows="2" ></textarea>
+                <textarea ref={text} className="inp-msg" cols="5" rows="2" ></textarea>
                 <div className="bar"></div>
-                <button type="button" className="btn_send">
+                <button onClick={()=> handleClick(text.current)}  type="button"  className="btn_send">
                     <FontAwesomeIcon icon={icon.faPaperPlane} />
                 </button>
             </div>
+
         </div>
     )
 }
