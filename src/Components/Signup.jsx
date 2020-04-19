9d5bd4e9505ddd2af2ecdba882550326dfd06056
Katuishi <diogenes_mj@hotmail.com>
2020-04-18 23:01:16 -0400

    fea: check username and use cookies

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import {  actionType} from "../Redux/typeAction";
@@ -9,6 +9,8 @@ export const Signup = (_) => {
   const dispatch = useDispatch()
   const history = useHistory()
 
+  
+
   const signUpUser = (username) => dispatch(()=>{
     dispatch({
       type: actionType.SIGN_UP_USER,
@@ -16,22 +18,64 @@ export const Signup = (_) => {
     })
   })
 
-  const get_username = (e) =>{
+  const showAlert = _=> dispatch(()=>{
+    dispatch({
+      type: actionType.STATE_ALERT,
+      payload:{
+        title:"Check your username please!",
+        visible: "visible"
+      }
+    })
+  })
+
+
+
+  const checkInWordObs = word =>{
+
+    let listWord = ['mmgvo','mamaguebo','mmguebazo','m.m.g','mama.guebo']
+    return !listWord.includes(word.toLowerCase())
+    
+  }
+
+  const handleOnSubmit = (e) =>{
 
     e.preventDefault()
     let username  = e.target.username.value.trim()
 
-    if(username)
+    if(username && username.length > 4 && username.length < 18 && checkInWordObs(username))
     {
       signUpUser(username)
+      localStorage.setItem('username',username)
       history.push('/chat')
-    }else{
-      alert('Check your username please!')
+
+    }
+    else
+    {
+        showAlert()
     }
 
   }
+
+  useEffect(()=>{
+    const loadUsername = _ =>{
+        let username = localStorage.getItem('username')
+        if(username){
+          signUpUser(username)
+          history.push('/chat')
+        }
+      }
+
+      loadUsername()
+
+      window.addEventListener('beforeunload',loadUsername)
+
+      return ()=>{
+        window.removeEventListener('beforeunload',loadUsername)
+      }
+    })
+
   return (
-    <form  onSubmit={(e)=> get_username(e)} className="form-sign-up" >
+    <form  onSubmit={(e)=> handleOnSubmit(e)} className="form-sign-up" >
       <p>Sign Up</p>
       <input name="username" minLength={"4"} maxLength = {"18"} placeholder="Type your nick" required/>
       <button>Sign Up</button>
