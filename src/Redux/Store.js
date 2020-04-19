f5a58df773b162819248768c7b31550c735e39e1
Katuishi <diogenes_mj@hotmail.com>
2020-04-19 09:32:08 -0400

    feat: modo Production.

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -2,14 +2,16 @@ import { createStore, applyMiddleware,compose } from "redux";
 import { chatReducer } from "./Reducer";
 import thunk from "redux-thunk";
 
+
+// Develoment
 // export const chatStore = createStore(
 //         chatReducer,
 //         compose(    
 //             applyMiddleware(thunk),
-//             window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()),
-        
+//             window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()),        
 // );
 
+// production
 export const chatStore = createStore(
     chatReducer,
     compose(    
